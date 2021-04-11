96f0ca22d985059922d12e46e1f2646fdaac2442
gilbertopapa <gilbertopapa013@hotmail.com>
2021-04-11 16:25:49 -0300

    create fragment base.

---
+++ ui/src/main/java/com/gilbertopapa/ui/BaseViewBindingFragment.kt
@@ -0,0 +1,29 @@
+package com.gilbertopapa.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseViewBindingFragment<Binding : ViewBinding> : Fragment() {
+    private lateinit var _binding: Binding
+
+    val binding: Binding get() = _binding
+
+    abstract val inflater: (LayoutInflater, ViewGroup?, Boolean) -> Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflater(inflater, container, false)
+        return _binding.root
+    }
+
+    protected fun getNavControllerFragment(fragment: Fragment) =
+        NavHostFragment.findNavController(fragment)
+}
